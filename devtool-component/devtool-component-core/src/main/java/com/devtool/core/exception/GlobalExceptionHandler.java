9d9001994eb6d2befb08ccf64e63d27e22e5b543
xzh <xiongzihao_xzh@163.com>
2023-11-20 23:47:39 +0800

    ✨ feat: 异常处理

---
+++ devtool-component/devtool-component-core/src/main/java/com/devtool/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.devtool.core.exception;
+
+import com.devtool.core.result.CommonResult;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理
+ * 
+ * @author <a href="mailto:xiongzihao_xzh@163.com">xzh</a>
+ * @date 2023/11/20
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 业务异常处理
+     */
+    @ExceptionHandler(ServiceException.class)
+    public CommonResult<?> serviceExceptionHandler(HttpServletRequest request, ServiceException ex) {
+        log.error("[ex]: {}", ex.toString());
+
+        return CommonResult.failed(ex.getCode(), ex.getMessage());
+    }
+
+    /**
+     * 异常捕获兜底
+     */
+    @ExceptionHandler(Throwable.class)
+    public CommonResult<?> serviceExceptionHandler(HttpServletRequest request, Throwable ex) {
+        log.error("[ex]: {}", ex.toString());
+
+        return CommonResult.failed();
+    }
+}
---
+++ devtool-component/devtool-component-core/src/main/java/com/devtool/core/exception/ServiceException.java
@@ -0,0 +1,29 @@
+package com.devtool.core.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author <a href="mailto:xiongzihao_xzh@163.com">xzh</a>
+ * @date 2023/11/20
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class ServiceException extends RuntimeException {
+
+    private int code;
+    private String message;
+
+    public ServiceException(ErrorCode errorCode) {
+        this.code = errorCode.code();
+        this.message = errorCode.message();
+    }
+
+    public ServiceException(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
